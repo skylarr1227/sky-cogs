edb7c1dadc383b29e38636d1b29bb7b042943fdc
skylarr1227 <44381751+skylarr1227@users.noreply.github.com>
2019-12-31 14:42:01 -0500

    Add files via upload

---
+++ dex/ext/utils.py
@@ -0,0 +1,34 @@
+import discord
+from discord.ext import commands
+import random
+import json
+
+
+
+def paginate(text: str):
+    '''Simple generator that paginates text.'''
+    last = 0
+    pages = []
+    for curr in range(0, len(text)):
+        if curr % 1980 == 0:
+            pages.append(text[last:curr])
+            last = curr
+            appd_index = curr
+    if appd_index != len(text) - 1:
+        pages.append(text[last:curr])
+    return list(filter(lambda a: a != '', pages))
+
+
+def cleanup_code(content):
+    '''Automatically removes code blocks from the code.'''
+    # remove ```py\n```
+    if content.startswith('```') and content.endswith('```'):
+        return '\n'.join(content.split('\n')[1:-1])
+    return content.strip('` \n')
+
+
+def random_color():
+    color = ('#%06x' % random.randint(8, 0xFFFFFF))
+    color = int(color[1:], 16)
+    color = discord.Color(value=color)
+    return color

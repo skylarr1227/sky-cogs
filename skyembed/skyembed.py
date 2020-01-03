9f74a3a1b58739465cb486a2a443655e208db9b5
skylarr1227 <44381751+skylarr1227@users.noreply.github.com>
2020-01-03 17:15:40 -0500

    Update skyembed.py

--- skyembed/skyembed.py
+++ skyembed/skyembed.py
@@ -142,7 +142,7 @@ class Skyembed(commands.Cog):
     @commands.command(name='embedly', description='embed image test #1')
     async def testimgembed(self, ctx):
         embed = discord.Embed(title="test test test", colour=discord.Colour(0x12bdca), description="")
-        embed.set_image(url="https://raw.githubusercontent.com/tdmalone/pokecss-media/master/graphics/pokemon/ani-front/kyurem-white.gif")
+        embed.set_image(url="https://raw.githubusercontent.com/tdmalone/pokecss-media/master/graphics/pokemon/ani-front/caterpie.gif")
         embed.set_thumbnail(url="https://pokepla.net/epic.gif")
         embed.set_author(name="Mega Test", url="http://pokepla.net", icon_url="https://go.goodguitarist.com/hosted/images/78/538b29231e4c1ab2c02cb6b8dc24b1/blue-arrows-flashing.gif")
       #  embed.set_footer(text="Suggestions on how to make this better? DM Skylarr#6666!!", icon_url="https://pokepla.net/epic2.gif")

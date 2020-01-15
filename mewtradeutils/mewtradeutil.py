e3828933e2dc6804451defc96d42911addbe7399
skylarr1227 <44381751+skylarr1227@users.noreply.github.com>
2020-01-14 22:54:59 -0500

    Update mewtradeutil.py

--- mewtradeutils/mewtradeutil.py
+++ mewtradeutils/mewtradeutil.py
@@ -11,18 +11,16 @@ class Mewtradeutil(commands.Cog):
 	def __init__(self, bot):
 		self.bot = bot
 		
-		
-	@commands.command(name='tradeutil')
-	async def trade(ctx, msg_id):
-	        """Trade Utility for Mewbot"""
-		m = await channel.fetch_message(msg_id)
-		lines = m.embeds[0].description.split('\n')
-		result = []
-		for line in lines:
-			start = line.find('**__No.__** - ')
-			sub = line[start + 14:]
-			end = sub.find('|')
-			final = sub[:end - 1]
-			result.append(int(final))
-		return result
-		
+@commands.command(name='tradeutil')
+async def trade(ctx, msg_id):
+    """Trade Utility for Mewbot"""
+    m = await channel.fetch_message(msg_id)
+    lines = m.embeds[0].description.split('\n')
+    result = []
+    for line in lines:
+        start = line.find('**__No.__** - ')
+        sub = line[start + 14:]
+        end = sub.find('|')
+        final = sub[:end - 1]
+        result.append(int(final))
+    return result

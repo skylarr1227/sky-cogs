df8d1b368c2546cda98c1e6d3e2ced8d1a03ddc2
skylarr1227 <44381751+skylarr1227@users.noreply.github.com>
2020-01-15 00:15:36 -0500

    Update mewtradeutil.py

--- mewtradeutils/mewtradeutil.py
+++ mewtradeutils/mewtradeutil.py
@@ -12,9 +12,9 @@ class Mewtradeutil(commands.Cog):
 		self.bot = bot
 		
 @commands.command(name='tradeutil')
-async def trade(ctx, msg_id):
+async def trade(ctx, msg_id: int):
 	"""Trade Utility for Mewbot"""
-	m = await channel.fetch_message(msg_id)
+	m = await ctx.channel.fetch_message(msg_id)
 	lines = m.embeds[0].description.split('\n')
 	result = []
 	for line in lines:

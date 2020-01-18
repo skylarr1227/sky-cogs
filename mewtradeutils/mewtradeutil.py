74ab335ac06a0d4214ccf67ec663903b6b8dfe3b
skylarr1227 <44381751+skylarr1227@users.noreply.github.com>
2020-01-17 21:46:06 -0500

    Update mewtradeutil.py

--- mewtradeutils/mewtradeutil.py
+++ mewtradeutils/mewtradeutil.py
@@ -11,20 +11,20 @@ class Mewtradeutil(commands.Cog):
 	def __init__(self, bot):
 		self.bot = bot
 		
-@commands.command(name='tradeutil')
-async def trade(ctx, msg_id: int):
-	"""Trade Utility for Mewbot"""
-	m = await ctx.channel.fetch_message(msg_id)
-	lines = m.embeds[0].description.split('\n')
-	result = []
-	for line in lines:
-		start = line.find('**__No.__** - ')
-		sub = line[start + 14:]
-		end = sub.find('|')
-		final = sub[:end - 1]
-		result.append(int(final))
-	embed = discord.Embed(title = "ID's requested", description=result, color=0xEE8700)
-	await ctx.send(embed=embed)
+	@commands.command()
+	async def trade(ctx, msg_id: int):
+		"""Trade Utility for Mewbot"""
+		m = await ctx.channel.fetch_message(msg_id)
+		lines = m.embeds[0].description.split('\n')
+		result = []
+		for line in lines:
+			start = line.find('**__No.__** - ')
+			sub = line[start + 14:]
+			end = sub.find('|')
+			final = sub[:end - 1]
+			result.append(int(final))
+		embed = discord.Embed(title = "ID's requested", description=result, color=0xEE8700)
+		await ctx.send(embed=embed)
 
 
 

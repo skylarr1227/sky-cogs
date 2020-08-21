589cd6f1b128ba9ab078759eeb3a018d2d84452c
Flame442 <34169552+Flame442@users.noreply.github.com>
2020-08-20 22:43:01 -0400

    Don't try to remove managed roles

--- skychill/chillzone.py
+++ skychill/chillzone.py
@@ -57,9 +57,10 @@ class skychill(commands.Cog):
         if not chillzone_role_obj:
             return await ctx.send("No chillzone role set.")
 
+        new_roles = [r for r in ctx.message.author if r.managed]
         try:
             await user.edit(
-                roles=[], reason=f"Removing all roles, {ctx.message.author} is banishing user"
+                roles=new_roles, reason=f"Removing all roles, {ctx.message.author} is banishing user"
             )
         except discord.Forbidden:
             return await ctx.send(

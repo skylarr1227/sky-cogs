e9c444745a4716150e1d701b28605fbae71db9c9
skylarr1227 <44381751+skylarr1227@users.noreply.github.com>
2021-04-09 01:43:58 -0400

    Update switch.py

--- switch/switch.py
+++ switch/switch.py
@@ -3,25 +3,27 @@ from redbot.core import commands
 from redbot.core import Config
 
 
-def __init__(self, bot):
-  self.config = Config.get_conf(self, identifier=790722073248661525)
-  self.config.register_user(
-    switch_code = None,
-  )
+class switch(commands.Cog):
+	"""Create auctions for Mewbot pokemon."""
+  def __init__(self, bot):
+    self.config = Config.get_conf(self, identifier=790722073248661525)
+    self.config.register_user(
+      switch_code = None,
+    )
 
-@commands.command()
-async def setswitch(self, ctx, code: str):
-  """Save your Nintendo Swich friend code for quick retrival."""
-  await self.config.user(ctx.author).switch_code.set(code)
-  embed = discord.Embed(title= "Successfull", description=f"Your Nintendo Switch invite code has been registered.\nCode:`{code}`", color=0xEE8700)
-  await ctx.send(embed=embed)
+  @commands.command()
+  async def setswitch(self, ctx, code: str):
+    """Save your Nintendo Swich friend code for quick retrival."""
+    await self.config.user(ctx.author).switch_code.set(code)
+    embed = discord.Embed(title= "Successfull", description=f"Your Nintendo Switch invite code has been registered.\nCode:`{code}`", color=0xEE8700)
+    await ctx.send(embed=embed)
 
-@commands.command()
-async def getswitch(self, ctx, member: discord.Member):
-  """Show targeted user's Nintendo Switch friend ID/Invite code if they have one registered."""
-  code = await self.config.user(member).switch_code()
-  if code is None:
-    await ctx.send("This user does not have their switch invite code registered.")
-    return
-  embed = discord.Embed(title= f"Users Invite Code:", description=f"`{code}`", color=0xEE8700)
-  await ctx.send(embed=embed)
+  @commands.command()
+  async def getswitch(self, ctx, member: discord.Member):
+    """Show targeted user's Nintendo Switch friend ID/Invite code if they have one registered."""
+    code = await self.config.user(member).switch_code()
+    if code is None:
+      await ctx.send("This user does not have their switch invite code registered.")
+      return
+    embed = discord.Embed(title= f"Users Invite Code:", description=f"`{code}`", color=0xEE8700)
+    await ctx.send(embed=embed)
